99a28bce6cfc8bd537417bfa6bccc328aa058e6e
horsicq <horsicq@gmail.com>
2022-02-16 20:33:00 +0100

    Fix: 2022-02-16

--- xpdf.cmake
+++ xpdf.cmake
@@ -2,7 +2,7 @@ include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xbinary.cmake)
 
-set(XDEX_SOURCES
+set(XPDF_SOURCES
     ${XBINARY_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xpdf.cpp
 )
