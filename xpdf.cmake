762278cdb1ee6e27c25b9a1680dae7f2792813a7
horsicq <horsicq@gmail.com>
2024-02-28 19:07:39 +0100

    Update file(s): 2024-02-28

--- xpdf.cmake
+++ xpdf.cmake
@@ -5,4 +5,6 @@ include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xbinary.cmake)
 set(XPDF_SOURCES
     ${XBINARY_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xpdf.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xpdf.h
+    ${CMAKE_CURRENT_LIST_DIR}/xpdf_def.h
 )
